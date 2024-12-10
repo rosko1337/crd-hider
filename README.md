1aa075a8db1412af46ec4471f593a4a0da6c2751
Oleg F. <roskodeadboy@gmail.com>
2024-12-10 14:09:28 +0300

    get rid of msdetours. added safetyhook. small code cleanup

--- README.md
+++ README.md
@@ -6,12 +6,17 @@ App written in C, DLL written in C++.
 ## Sources
 - [tray](https://github.com/zserge/tray)  
 - [safetyhook](https://github.com/cursey/safetyhook)  
+- [zydis](https://github.com/zyantific/zydis)
+- [plugin-sdk](https://github.com/DK22Pac/plugin-sdk) (safetyhook, zydis)  
 - [icon](https://www.flaticon.com/free-icon/laptop_955032)  
 - stackoverflow topics
 
 ## Demo
 [![Demo](https://img.youtube.com/vi/oI32j0lZVA8/hqdefault.jpg)](https://www.youtube.com/watch?v=oI32j0lZVA8)  
 ![Menu](https://i.imgur.com/BsjWzYH.png)
+
+## Todo
+use git submodules
 
 ## License
 [BSL-1.0](https://choosealicense.com/licenses/bsl-1.0/)
--- crd_hider/main.cpp
+++ crd_hider/main.cpp
@@ -1,195 +1,158 @@
 #include "main.h"
 
-#include "detours/detours.h"
-#pragma comment(lib, "detours/detours.lib")
+#include "safetyhook\safetyhook.hpp"
 
-HANDLE  g_mutex = nullptr;
-bool    g_unloading = false;
+std::atomic_bool    g_unloading = false;
+HANDLE              g_mutex = nullptr;
+SafetyHookInline    g_hookShowWindow{};
+SafetyHookInline    g_hookAnimateWindow{};
+SafetyHookInline    g_hookIsWindowVisible{};
 
 // https://stackoverflow.com/questions/20162359/c-best-way-to-get-window-handle-of-the-only-window-from-a-process-by-process/20163705#20163705
+namespace helpers
+{
+    struct EnumData {
+        DWORD dwProcessId;
+        HWND hWnd;
+    };
 
-struct EnumData {
-    DWORD dwProcessId;
-    HWND hWnd;
-};
-
-BOOL CALLBACK EnumProc(HWND hWnd, LPARAM lParam) {
-    // Retrieve storage location for communication data
-    EnumData& ed = *(EnumData*)lParam;
-    DWORD dwProcessId = 0x0;
-    // Query process ID for hWnd
-    GetWindowThreadProcessId(hWnd, &dwProcessId);
-    // Apply filter - if you want to implement additional restrictions,
-    // this is the place to do so.
-    if (ed.dwProcessId == dwProcessId) {
-        // Found a window matching the process ID
-        ed.hWnd = hWnd;
-        // Report success
-        SetLastError(ERROR_SUCCESS);
-        // Stop enumeration
-        return FALSE;
+    BOOL CALLBACK EnumProc(HWND hWnd, LPARAM lParam) {
+        EnumData& ed = *(EnumData*)lParam;
+        DWORD dwProcessId = 0x0;
+        GetWindowThreadProcessId(hWnd, &dwProcessId);
+        if (ed.dwProcessId == dwProcessId) {
+            ed.hWnd = hWnd;
+            SetLastError(ERROR_SUCCESS);
+            return FALSE;
+        }
+        return TRUE;
     }
-    // Continue enumeration
+
+    HWND FindWindowFromProcessId(DWORD dwProcessId) {
+        EnumData ed = { dwProcessId, 0 };
+        if (!EnumWindows(EnumProc, (LPARAM)&ed) &&
+            ((GetLastError() == ERROR_SUCCESS) || ed.hWnd != 0)) {
+            return ed.hWnd;
+        }
+        return NULL;
+    }
 }
 
-HWND FindWindowFromProcessId(DWORD dwProcessId) {
-    EnumData ed = { dwProcessId, 0 };
-    if (!EnumWindows(EnumProc, (LPARAM)&ed) &&
-        ((GetLastError() == ERROR_SUCCESS) || ed.hWnd != 0)) {
-        return ed.hWnd;
-    }
-    return NULL;
-}
-
-// структура дл€ хранени€ необходимых указателей и адресов
-struct user32_dll {
-    HMODULE dll;
-    FARPROC Orignal_ShowWindow;
-    FARPROC Orignal_AnimateWindow;
-    FARPROC Orignal_IsWindowVisible;
-} user32;
-
-using ShowWindowFn = BOOL(__stdcall*)(HWND, int);
 BOOL __stdcall hooked_ShowWindow(HWND hWnd, int nCmdShow)
 {
-    // объ€вление оригинальной функции ShowWindow
-    auto original = reinterpret_cast<ShowWindowFn>(user32.Orignal_ShowWindow);
-
-    // подмена значени€ аргумента
+    // replace input value
     if (nCmdShow == SW_SHOW)
         nCmdShow = SW_HIDE;
 
-    // вызов оригинальной функции
-    auto return_value = original(hWnd, nCmdShow);
-
-    // возвращаем значение вызова
-    return return_value;
+    // call original and return a value
+    return g_hookShowWindow.stdcall<BOOL, HWND, int>(hWnd, nCmdShow);
 }
 
-using AnimateWindowFn = BOOL(__stdcall*)(HWND, DWORD, DWORD);
 BOOL __stdcall hooked_AnimateWindow(HWND hWnd, DWORD dwTime, DWORD dwFlags)
 {
-    // объ€вление оригинальной функции AnimateWindow
-    auto original = reinterpret_cast<AnimateWindowFn>(user32.Orignal_AnimateWindow);
-
-    // подмена возвращаемого значени€ вызова функции, без вызова оригинала, иначе - краш. подмена флагов тоже ведет к крашу.
-    auto return_value = TRUE;
-
-    // возвращаем значение вызова
-    return return_value;
+    // just return a value
+    return TRUE;
 }
 
-using IsWindowVisibleFn = BOOL(__stdcall*)(HWND);
 BOOL __stdcall hooked_IsWindowVisible(HWND hWnd)
 {
-    // объ€вление оригинальной функции IsWindowVisible
-    auto original = reinterpret_cast<IsWindowVisibleFn>(user32.Orignal_IsWindowVisible);
+    // call original
+    g_hookIsWindowVisible.stdcall<BOOL, HWND>(hWnd);
 
-    // вызов оригинальной функции
-    auto return_value = original(hWnd);
-
-    // подмена возвращенного значени€ вызова функции
-    return_value = TRUE;
-
-    // возвращаем значение вызова
-    return return_value;
+    // and return a value
+    return TRUE;
 }
-
-/*struct gdi32_dll {
-    HMODULE dll;
-    FARPROC Orignal_ScriptTextOut;
-} gdi32;
-
-using ScriptTextOutFn = HRESULT(__stdcall*)(const HDC, void*, int, int, UINT, const RECT*, const void*, WCHAR*, int, const WORD*, int, const int*, const int*, const void*);
-HRESULT __stdcall hooked_ScriptTextOut(const HDC hdc, void* psc, int x, int y, UINT fuOptions, const RECT* lprc, const void* psa, WCHAR* pwcReserved, int iReserved, const WORD* pwGlyphs, int cGlyphs, const int* piAdvance, const int* piJustify, const void* pGoffset)
-{
-    auto original = reinterpret_cast<ScriptTextOutFn>(gdi32.Orignal_ScriptTextOut);
-    y = 10;
-    auto return_value = original(hdc, psc, x, y, fuOptions, lprc, psa, pwcReserved, iReserved, pwGlyphs, cGlyphs, piAdvance, piJustify, pGoffset);
-    return return_value;
-}*/
 
 void main_thread(HMODULE hModule)
 {
+    // we need only one instance
     g_mutex = CreateMutex(NULL, FALSE, L"crd_dll");
     if (g_mutex != 0 && GetLastError() == ERROR_ALREADY_EXISTS)
     {
-        MessageBox(0, L"ogo", L"ogo", MB_OK | MB_ICONERROR);
+        // throw a strange messagebox
+        MessageBox(0, L"crd_hider.dll already loaded?", L"crd_hider", MB_OK | MB_ICONERROR);
+        // exit
         return;
     }
 
-    // ждем загрузки user32.dll, если он еще на загрузилс€
+    // waiting for user32.dll to load, if it is not loaded
     while (!GetModuleHandle(L"user32.dll"))
         Sleep(100);
 
-    // получаем указатель на user32.dll
-    user32.dll = GetModuleHandle(L"user32.dll");
-    if (user32.dll == nullptr) // провер€ем получили ли мы указатель, и если нет, то ->
+    // get user32.dll address
+    auto user32_dll = GetModuleHandle(L"user32.dll");
+    if (user32_dll == nullptr) // if cant get user32.dll address (?) -> exit
     {
-        // ждем 3 секунды
-        Sleep(3000);
-        // освобождаем библиотеку и завершаем поток
-        FreeLibraryAndExitThread(hModule, 0);
+        // throw a stupid messagebox
+        MessageBox(0, L"GetModuleHandle failed", L"crd_hider", MB_OK | MB_ICONERROR);
+        // exit
+        return;
     }
 
-    // ищем указатель окна по id текущего процесса
-    auto hwnd = FindWindowFromProcessId(GetCurrentProcessId());   
-    // если найденное окно видно
+    // get hwnd by process id
+    auto hwnd = helpers::FindWindowFromProcessId(GetCurrentProcessId());
+
+    // if window is visible
     if (IsWindowVisible(hwnd))
-        // скрываем найденное окно с анимацией
+        // hide a window with animation
         AnimateWindow(hwnd, 0xC8, AW_BLEND | AW_HIDE);
-    // полностью скрываем окно
-    ShowWindow(hwnd, 0);
 
-    // из библиотеки user32.dll получаем адрес функции ShowWindow
-    user32.Orignal_ShowWindow = GetProcAddress(user32.dll, "ShowWindow");
-    user32.Orignal_AnimateWindow = GetProcAddress(user32.dll, "AnimateWindow");
-    user32.Orignal_IsWindowVisible = GetProcAddress(user32.dll, "IsWindowVisible");
+    // hide a window
+    ShowWindow(hwnd, SW_HIDE);
 
-    //gdi32.dll = GetModuleHandle(L"gdi32full.dll");
-    //gdi32.Orignal_ScriptTextOut = GetProcAddress(gdi32.dll, "ScriptTextOut");
+    // from user32.dll get addr of ShowWindow, AnimateWindow and IsWindowVisible
+    auto show_window_fn = GetProcAddress(user32_dll, "ShowWindow");
+    auto animate_window_fn = GetProcAddress(user32_dll, "AnimateWindow");
+    auto is_window_visible_fn = GetProcAddress(user32_dll, "IsWindowVisible");
 
-    // перехватываем функции ShowWindow, AnimateWindow и IsWindowVisible дл€ управлени€ их входными и выходными данными 
-    DetourTransactionBegin();
-    DetourUpdateThread(GetCurrentThread());
-    DetourAttach(&reinterpret_cast<void*&>(user32.Orignal_ShowWindow), &hooked_ShowWindow);
-    DetourAttach(&reinterpret_cast<void*&>(user32.Orignal_AnimateWindow), &hooked_AnimateWindow);
-    DetourAttach(&reinterpret_cast<void*&>(user32.Orignal_IsWindowVisible), &hooked_IsWindowVisible);
-    //DetourAttach(&reinterpret_cast<void*&>(gdi32.Orignal_ScriptTextOut), &hooked_ScriptTextOut);
-    DetourTransactionCommit();
+    if (show_window_fn == nullptr || animate_window_fn == nullptr || is_window_visible_fn == nullptr)
+    {
+        // throw a simple messagebox
+        MessageBox(0, L"GetProcAddress failed", L"crd_hider", MB_OK | MB_ICONERROR);
+        // exit
+        return;
+    }
+
+    // hook them
+    g_hookShowWindow = safetyhook::create_inline(reinterpret_cast<void*>(show_window_fn), reinterpret_cast<void*>(hooked_ShowWindow)); //safetyhook::create_inline(show_window_fn, hooked_ShowWindow);
+    g_hookAnimateWindow = safetyhook::create_inline(reinterpret_cast<void*>(animate_window_fn), reinterpret_cast<void*>(hooked_AnimateWindow)); //safetyhook::create_inline(animate_window_fn, hooked_AnimateWindow);
+    g_hookIsWindowVisible = safetyhook::create_inline(reinterpret_cast<void*>(is_window_visible_fn), reinterpret_cast<void*>(hooked_IsWindowVisible)); //safetyhook::create_inline(is_window_visible_fn, hooked_IsWindowVisible);
 
     while (!g_unloading) {
-        Sleep(1000);
+        Sleep(3000);
     }
 }
 
 BOOL WINAPI DllMain(HMODULE hModule, DWORD dwReason, LPVOID lpReserved)
 {
-    if (dwReason == DLL_PROCESS_ATTACH)
+    switch (dwReason)
     {
+    case DLL_PROCESS_ATTACH:
         DisableThreadLibraryCalls(hModule);
 
-        CreateThread(0, 0, (LPTHREAD_START_ROUTINE)main_thread, hModule, 0, 0);
-    }
-    else if (dwReason == DLL_PROCESS_DETACH)
-    {
+        if (CreateThread(0, 0, (LPTHREAD_START_ROUTINE)main_thread, hModule, 0, 0) == NULL)
+        {
+            // just throw a messagebox
+            MessageBox(0, L"CreateThread failed", L"crd_hider", MB_OK | MB_ICONERROR);
+            // exit
+            FreeLibraryAndExitThread(hModule, 0);
+        }
+        break;
+    case DLL_PROCESS_DETACH:
         g_unloading = true;
-        Sleep(100);
+        Sleep(3000);
 
         if (g_mutex != 0)
             CloseHandle(g_mutex);
 
-        // отдаем управление функци€ми ShowWindow и AnimateWindow обратно
-        DetourTransactionBegin();
-        DetourUpdateThread(GetCurrentThread());
-        DetourDetach(&reinterpret_cast<void*&>(user32.Orignal_ShowWindow), &hooked_ShowWindow);
-        DetourDetach(&reinterpret_cast<void*&>(user32.Orignal_AnimateWindow), &hooked_AnimateWindow);
-        DetourDetach(&reinterpret_cast<void*&>(user32.Orignal_IsWindowVisible), &hooked_IsWindowVisible);
-        //DetourDetach(&reinterpret_cast<void*&>(gdi32.Orignal_ScriptTextOut), &hooked_ScriptTextOut);
-        DetourTransactionCommit();
+        // unhook
+        g_hookShowWindow = {};
+        g_hookAnimateWindow = {};
+        g_hookIsWindowVisible = {};
 
-        // освобождаем библиотеку и завершаем поток
+        // exit
         FreeLibraryAndExitThread(hModule, 0);
+        break;
     }
 
     return TRUE;
